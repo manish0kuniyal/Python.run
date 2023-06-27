ef1bc01c623d99315cd642c30206b727250ec622
Manish Kuniyal <110035752+manish0kuniyal@users.noreply.github.com>
2023-06-27 14:19:10 +0530

    Update and rename loo_p.py to synt_ax.py

--- synt_ax.py
+++ synt_ax.py
@@ -82,3 +82,55 @@ for i in range (k):
             print("*",end='')
 
         print()
+
+
+
+///FUNCTIONS///
+
+
+# INDENTATION - spaces in the beginning of a statement
+# no. of statement is up to you but atleast one
+if(5>2):
+         print('okokok')
+
+
+def func():
+    print('a funcn')
+func()
+func()
+
+# parameter
+def func2(par):
+    print('a',par)
+func2("okk")
+func2(2)
+
+def func3(par,val):
+    print(par+val)
+func3('o','k')
+func3(2,3)
+
+def func(no):
+    for i in range(0,5,+1):
+        print(i,end='')
+ar=[1,2,3,4]
+func(ar)
+
+
+# RECURSION
+def func(val):
+    print(val)
+    if(val<5):
+     func(val+1)
+
+func(2)
+
+def fun2():
+    for i in range(1,4,+1):
+        print('hello',i)
+def fun1():
+    print("func1")
+    fun2()
+
+fun1()
+
